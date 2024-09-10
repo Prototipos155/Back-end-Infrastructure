34238f2dc398649e685433fd4939e0437f7d1656
oalmed <osmedina04@gmail.com>
2024-09-09 23:43:28 -0600

    esconder/mostrar campos segun el rol

--- peertopeer/static/js/validacionRegistro.js
+++ peertopeer/static/js/validacionRegistro.js
@@ -1,12 +1,28 @@
-const roles = document.getElementsByName("rol");
-for (const item of roles) {
-    item.addEventListener("change", cambiar);
-}
+alumno = document.getElementById("alumno");
+tutor = document.getElementById("tutor");
+numTarjBien = document.getElementById("tarjetabien");
+inpTarjetaBien = document.getElementById("numTarjetaBien");
 
-function cambiar() {
-    if (roles[0].checked) {
-        roles[1].checked = false;
-    } else if (roles[1].checked) {
-        roles[0].checked = false;
+alumno.addEventListener("change", (e) => {
+    e.preventDefault();
+    if (!alumno.checked) {
+        alumno.checked = true;
     }
-}
+
+    tutor.checked = false;
+    inpTarjetaBien.required = false;
+    numTarjBien.hidden = true;
+    tutor.required = false;
+});
+
+tutor.addEventListener("change", (e) => {
+    e.preventDefault();
+    if (!tutor.checked) {
+        tutor.checked = true;
+    }
+
+    alumno.checked = false;
+    alumno.required = false;
+    inpTarjetaBien.required = true;
+    numTarjBien.hidden = false;
+});
