7b0ac4f626af4b8bd7a39b5e0f06b1d81cbdf891
Carlos Arias <carlosariassnadoval34@gmail.com>
2024-09-13 12:57:39 -0600

    restauracion e integracion de faltantes

--- peertopeer/app.py
+++ peertopeer/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("principal.html")
+    return render_template("home.html")
 
 @app.route ('/iniciarSesion')
 def iniciarSesion():
