d187a2253e84d9b9272024d6f68a1fab79098b17
Carlos Arias <carlosariassnadoval34@gmail.com>
2024-09-14 15:54:17 -0600

    correcion de errores en archivos

--- peertopeer/app.py
+++ peertopeer/app.py
@@ -1,23 +1,26 @@
-<<<<<<< HEAD
-from flask import Flask, render_template,uest, redirect, url_for, flash
-=======
-from flask import Flask, render_template #,uest, redirect, url_for, flash
-from db.admin import Admin
->>>>>>> DB/main
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
-=======
+
     a=Admin()
     # a.logInAlumn('22301061553360','22301061553360@cetis155.edu.mx','7839123848','contraseñ','pepito',3,'A')
     # a.logInTutor('22301061553361','22301061553361@cetis155.edu.mx','7839120048','contraseñ','oscar','guzaman alvedo','1234123340989301','josefa ortiz de dominguez','bachillerato')
     a.close()
-    
->>>>>>> DB/main
-    return render_template("principal.html")
+
+    return render_template("home.html")
+
+@app.route ('/iniciarSesion')
+def iniciarSesion():
+    return render_template("iniciosesi.html")
+
+@app.route ('/registro')
+def registro():
+    return render_template("registro.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
