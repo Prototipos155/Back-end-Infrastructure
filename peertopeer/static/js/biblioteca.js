3ef4413880b9591927852657a0f2cfae1e385ab1
Martin <22301061550038@cetis155.edu.mx>
2024-11-14 08:41:47 -0600

    otro avance en la biblioiteca

--- peertopeer/static/js/biblioteca.js
+++ peertopeer/static/js/biblioteca.js
@@ -3,7 +3,7 @@ let targ;
 for (let desplegable of desplegables){
     desplegable.addEventListener("click",e=>{
         targ=e;
-        menuActual=e.target.parentNode.lastElementChild
+        menuActual=e.target.parentNode.parentNode.lastElementChild
         console.log(menuActual)
         if(menuActual.checkVisibility()){
             menuActual.style.display="none"
@@ -27,6 +27,21 @@ for (let desplegable of desplegables){
 
 for(let puntos of document.querySelectorAll(".puntos")){
     puntos.addEventListener("click",e=>{
-        e.parentNode.lastElementChild.style.display="block"
+        targ=e
+        console.log("targ=",e.target)
+        console.log("padre=",e.target.parentNode)
+        console.log("hermano=",e.target.parentNode.lastElementChild)
+
+        if(e.target.parentNode.lastElementChild.checkVisibility()){
+            e.target.parentNode.lastElementChild.style.display="none"
+        }else{
+            e.target.parentNode.lastElementChild.style.display="block"
+        }
+    })
+    puntos.parentNode.lastElementChild.lastElementChild.addEventListener("click",e=>{
+        if(e.stopPropagation){
+            e.stopPropagation()
+        }
+        e.target.parentNode.style.display="none"
     })
 }
